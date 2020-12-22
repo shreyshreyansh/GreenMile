51f56f242be0b5060cf85cf68c9b1794a3bea242
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2020-12-22 14:42:46 +0530

    v0.0.2

--- index.js
+++ index.js
@@ -31,3 +31,20 @@
       } 
     });
   });
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
